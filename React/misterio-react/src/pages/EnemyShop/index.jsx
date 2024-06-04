87e0f773f63e2d268d5bdc5329625cbccf32cddc
Gustavo <gv.pinheiro@outlook.com>
2024-06-04 09:29:22 -0300

    feat: aula de paginação

---
+++ React/misterio-react/src/pages/EnemyShop/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const EnemyShop = () => {
+    return <div>
+        <div>
+            Vilões 1
+        </div>
+        <div>
+            Vilões 2
+        </div>
+        <div>
+            Vilões 3
+        </div>
+        <div>
+            Vilões 4
+        </div>
+    </div>
+}
--- React/misterio-react/src/pages/Login/App.jsx
+++ React/misterio-react/src/pages/Login/App.jsx
@@ -5,27 +5,27 @@ function App() {
   const [count, setCount] = useState(0);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
-  function addCount(){
-      setCount(count => count + 1)
-    };
 
-    useEffect (() => {
-      console.log("password", password)
-      console.log("email", email) 
-    }, [count]);
-  
+  function addCount() {
+    setCount(count => count + 1)
+  };
+
+  useEffect(() => {
+    console.log("password", password)
+    console.log("email", email)
+  }, [count]);
+
   return (
-      <div className={styles.container}>
-        <p>Email</p>
-        <input onChange={e => setEmail(e.target.value)}/>
-        <p>Senha</p>
-        <input onChange={e => setPassword(e.target.value)} />
-        <button onClick={addCount}>
-          Atualiza contagem
-        </button>
-      </div>
-    
+    <div className={styles.container}>
+      <p>Email</p>
+      <input onChange={e => setEmail(e.target.value)} />
+      <p>Senha</p>
+      <input onChange={e => setPassword(e.target.value)} />
+      <button onClick={addCount}>
+        Atualiza contagem
+      </button>
+    </div>
+
   )
 }
 
